ccae3f23645df9f448e39ad0b82940fb06580842
eduart <abdullahu.eduart@outlook.com>
2022-04-25 13:55:21 +0200

    basic changes
    -moved database functions from the farm.py to the db.py.
    -implemented new function to get data from DB.

--- farm/farm/db.py
+++ farm/farm/db.py
@@ -1,30 +1,51 @@
+import os
 import sqlite3
-
+from flask import Flask, request, session, g, redirect, url_for, abort,render_template, flash
 import click
-from flask import current_app, g
 from flask.cli import with_appcontext
 
+app = Flask(__name__) # create the application instance :)
+app.config.from_object(__name__) # load config from this file 
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'farm.db'),
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+
+app.config.from_envvar('FARM_SETTINGS', silent=True)
+
+def get_App():
+    return app
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
 def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
-
-    return g.db
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
 
 
-def close_db(e=None):
-    db = g.pop('db', None)
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
-    if db is not None:
-        db.close()
 
 def init_db():
     db = get_db()
 
-    with current_app.open_resource('schema.sql') as f:
+    with app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
 
@@ -39,3 +60,29 @@ def init_db_command():
 def init_app(app):
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
+
+
+
+
+
+
+'''-------------------------------------------------------------------------------------------------------------
+   ----------------------------------GETTERS AND SETTERS FOR DATA IN DATABASE-----------------------------------
+   -------------------------------------------------------------------------------------------------------------'''
+   
+   
+def get_from_db(name,table):
+    l=[]
+    try:
+        db=connect_db()
+        print(db)
+        cursor=db.cursor()
+        print(cursor)
+        for i in cursor.execute(f"SELECT {name}  from {table}"):
+            l.append(i[0])
+        l.sort()
+        return l
+    except:
+        return l
+    
+        
--- farm/farm/farm.py
+++ farm/farm/farm.py
@@ -1,56 +1,14 @@
 import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort,render_template, flash
-     
-
-app = Flask(__name__) # create the application instance :)
-app.config.from_object(__name__) # load config from this file , flaskr.py
-
-# Load default config and override config from an environment variable
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'farm.db'),
-    SECRET_KEY='development key',
-    USERNAME='admin',
-    PASSWORD='default'
-))
-app.config.from_envvar('FARM_SETTINGS', silent=True)
+from farm import db as database
 
 
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-def init_db():
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-
-@app.cli.command('init_db')
-def init_db_command():
-    """Initializes the database."""
-    init_db()
-    print('Initialized the database.')
-    
-
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-@app.teardown_appcontext
-def close_db(error):
-    """Closes the database again at the end of the request."""
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
+app=database.get_App()
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html",familles=database.get_from_db("nom","familles"))
 
 @app.route('/',methods=['POST','GET'])
 def graphics():
