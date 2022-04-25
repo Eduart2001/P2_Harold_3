27c0a1ce27bac676b3f152c5d39bcfc49a5cba18
IsaiZ734 <isaimola@gmail.com>
2022-04-25 14:36:46 +0200

    chart.js install

--- farm/farm/farm.py
+++ farm/farm/farm.py
@@ -1,7 +1,7 @@
 import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort,render_template, flash
-from farm import db as database
+from . import db as database
 
 
 app=database.get_App()
