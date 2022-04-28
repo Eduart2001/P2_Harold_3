cfb53a1c800d85ad78497735d3d30a7b25287ff0
Denos2729 <102650430+Denos2729@users.noreply.github.com>
2022-04-28 15:19:40 +0200

    types table added to the index function

--- farm/farm/farm.py
+++ farm/farm/farm.py
@@ -11,7 +11,7 @@ def index():
     keys=database.graph0().keys()
     values=database.graph0().values()
     print(keys,values)
-    return render_template("index.html",familles=database.get_from_db("nom","familles"),keys=keys,values=values)
+    return render_template("index.html",familles=database.get_from_db("nom","familles"),keys=keys,values=values,types=database.get_from_db("type","types"))
 
 @app.route('/',methods=['POST','GET'])
 def graphics():
